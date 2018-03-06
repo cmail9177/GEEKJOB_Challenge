b2c5d3675a093f75117a153c7a42de19907a5c87
gusikentatuya <cmail9177@gmail.com>
2018-03-06 13:30:50 +0900

    3/6提出

---
+++ CampChallenge_java/013.クラス/class2.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.camp.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+
+
+class num1{
+    public String num2 = "5";
+    public String num3 = "1";
+     public void setnum(String num5, String num6) {
+        this.num2 = num5; //メソッド
+        this.num3 = num6;
+    }
+    void AA(PrintWriter pw){
+        pw.print(num2+num3);
+    } 
+ }
+class num0 extends num1{
+    
+     void n(){
+         this.num2=null;
+         this.num3=null;
+     }
+}
+/**
+ *
+ * @author guest1Day
+ */
+@WebServlet(name = "class2", urlPatterns = {"/class2"})
+public class class2 extends HttpServlet {
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+       
+            
+      num0 sett=new num0();
+      
+      sett.setnum("a","ss");
+      sett.n();
+      
+      sett.AA(out);
+      
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet class1</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet class1 at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
