a7615d63d49b2cf7878c10cbe386637e38fe1477
gusikentatuya <cmail9177@gmail.com>
2018-03-06 12:57:18 +0900

    3/6日　提出

---
+++ CampChallenge_java/011.メソッド/Hikisuumodoriti1.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.camp.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author guest1Day
+ */
+@WebServlet(name = "innsuumodoriti1", urlPatterns = {"/innsuumodoriti1"})
+public class Hikisuumodoriti1 extends HttpServlet {
+
+       public String[] M(String test) {
+        if (test == "123") {
+            String[] aPUROF = {test,"1992", "神奈川県"};
+            return aPUROF;
+        }else{
+            String[] bPUROF = {"321", "1993", "横浜"};
+            String[] cPUROF = {"432", "2018", "栃木"};
+        }
+        return null;
+       }
+      
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            
+           String[] prof = M("123");
+//           String[] prof = bPUROF;
+         
+               for(int i =0 ;i <3 ; i++){
+                 out.print(prof[i]); 
+               }
+               
+//             out.print(prof[0]);
+//             out.print(prof[1]);
+//             out.print(prof[2]);
+
+
+
+
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet NewServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet NewServlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
